cee61cbf839d047f89d5c7b716a619ce1e20acb4
bathelomeo <ngethenathaniel@gmail.com>
2021-07-12 15:22:44 +0400

    first python

---
+++ test.py
@@ -0,0 +1,6 @@
+# first_name = 'Tyrone'
+# last_name = 'Kamau'
+first_name = input ('Please enter your first name :')
+last_name = input ('Please enter your last name :')
+# print (first_name + ' ' + last_name)
+print ('Hello' + ' ' + first_name.capitalize() + ' ' +last_name.capitalize())

8c8f8ee7000bd2bba944ee3f70570c88a7e1d89d
bathelomeo <ngethenathaniel@gmail.com>
2021-08-04 23:15:37 +0400

    updated

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 # pi = 3.14159
 # print(pi)
 
-first_num =  input ('Please enter a number')
-second_num = input ('Please enter a another number')
-print (int(first_num) + int(second_num))
+# first_num =  input ('Please enter a number')
+# second_num = input ('Please enter a another number')
+# print (int(first_num) + int(second_num))
 
-# days_in_June = 31
-# print(str(days_in_June) + ' ' +'total days in June')
-# from datetime import datetime
-# current_date = datetime.now()
-# print('Today is :'+ ' ' +str(current_date))
-# from datetime import datetime,timedelta
+days_in_June = 31
+print(str(days_in_June) + ' ' +'total days in June')
+from datetime import datetime
+current_date = datetime.now()
+print('Today is :'+ ' ' +str(current_date))
+from datetime import datetime,timedelta
+today = datetime.now()
+print('Today is :'+ ' ' +str(today))
+one_day = timedelta(days=1)
+yesterday = today - one_day
+print('Yesterday was :' + str(yesterday))

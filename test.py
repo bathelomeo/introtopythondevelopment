2b05ed376c6098bab8905ba73d3b73fc114f2617
bathelomeo <ngethenathaniel@gmail.com>
2021-08-06 14:36:58 +0400

    update

--- test.py
+++ test.py
@@ -5,14 +5,13 @@
 # second_num = input ('Please enter a another number')
 # print (int(first_num) + int(second_num))
 
-days_in_June = 31
-print(str(days_in_June) + ' ' +'total days in June')
+days_in_August = 31
+print(str(days_in_August) + ' ' +'total days in August')
 from datetime import datetime
 current_date = datetime.now()
 print('Today is :'+ ' ' +str(current_date))
 from datetime import datetime,timedelta
 today = datetime.now()
-print('Today is :'+ ' ' +str(today))
 one_day = timedelta(days=1)
 yesterday = today - one_day
 print('Yesterday was :' + str(yesterday))
